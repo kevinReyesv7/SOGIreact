f2b1624453b1d59958c2f2865d5633b6e8600836
dnorambu <65257235+dnorambu@users.noreply.github.com>
2020-08-11 16:55:26 -0400

    CopyPaste del git de OpelCorsa
    Se copia el sidebar que acordamos para la integración

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -1,14 +1,34 @@
 export default function() {
   return [
     {
-      title: 'Listar Equipamiento',
-      htmlBefore: '<i class="material-icons">note_add</i>',
-      to: '/listar-equipamientos'
+      title: "Solicitudes y reservas",
+      htmlBefore: '<i class="material-icons">vertical_split</i>',
+      to: "https://solrec-app.herokuapp.com/",
     },
     {
-      title: 'Añadir Equipamiento',
-      htmlBefore: '<i class="material-icons">note_add</i>',
-      to: '/anadir-equipamientos'
+      title: "Quimioterapia",
+      to: "/quimioterapia",
+      htmlBefore: '<i class="material-icons">edit</i>'
     },
+    {
+      title: 'Pabellones',
+      htmlBefore: '<i class="material-icons">view_module</i>',
+      to: 'https://iif-front.herokuapp.com/pabellones/'
+    },
+    {
+      title: 'Salas de recuperación',
+      htmlBefore: '<i class="material-icons">view_module</i>',
+      to: 'https://iif-front.herokuapp.com/recuperacion/'
+    },
+    {
+      title: 'Equipamientos',
+      htmlBefore: '<i class="material-icons">view_module</i>',
+      to: 'https://sogireact.herokuapp.com/listar-equipamientos/'
+    },
+    {
+      title: 'Personal y Equipos',
+      htmlBefore: '<i class="material-icons">note_add</i>',
+      to: 'https://chopinhauer-web.herokuapp.com/all-pservice'
+    }
   ];
 }
