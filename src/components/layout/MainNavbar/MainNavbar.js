0362281bfc2f7acaf90ed6c36b419d163db914d1
Diego Norambuena Vergara <diego.norambuena@sansano.usm.cl>
2020-08-11 21:26:43 -0400

    Cambios en navbar

--- src/components/layout/MainNavbar/MainNavbar.js
+++ src/components/layout/MainNavbar/MainNavbar.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import { Container, Navbar } from "shards-react";
 
-import NavbarSearch from "./NavbarSearch";
-import NavbarNav from "./NavbarNav/NavbarNav";
+import NavbarNavItems from "./NavbarNav/NavbarNavItems";
 import NavbarToggle from "./NavbarToggle";
+
 
 const MainNavbar = ({ layout, stickyTop }) => {
   const classes = classNames(
@@ -18,8 +18,7 @@ const MainNavbar = ({ layout, stickyTop }) => {
     <div className={classes}>
       <Container className="p-0">
         <Navbar type="light" className="align-items-stretch flex-md-nowrap p-0">
-          <NavbarSearch />
-          <NavbarNav />
+          <NavbarNavItems />
           <NavbarToggle />
         </Navbar>
       </Container>
